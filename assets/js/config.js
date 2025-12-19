761b664a8107ed183c053b4e5a6b2283af3b35f0
LeonardoEliasQuispeH <147902081+LeonardoEliasQuispeH@users.noreply.github.com>
2025-12-19 16:28:46 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 //const PROXY = "https://corsproxy.io/?";
 //const API_BASE_URL = PROXY + "https://lupalegiback-g6h0ckhdh2f3fcf3.canadacentral-01.azurewebsites.net/api/v1";
-//const API_BASE_URL = "https://lupalegiback-g6h0ckhdh2f3fcf3.canadacentral-01.azurewebsites.net/api/v1";
+const API_BASE_URL = "https://lupalegiback-g6h0ckhdh2f3fcf3.canadacentral-01.azurewebsites.net/api/v1";
